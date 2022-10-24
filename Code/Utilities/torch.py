0eb34edaa24668ee1d7fb8dff7a534c2df45ce43
giacomopiccinini <piccinini.giacomo@gmail.com>
2022-10-24 19:15:24 +0200

    [FIX] Fix typo

--- Code/Utilities/torch.py
+++ Code/Utilities/torch.py
@@ -5,6 +5,6 @@ def select_device(device='GPU', batch_size=None):
     """ Select correct device, i.e. CPU or (multi) GPU """
 
     # Ensure you have CPU if you selected it
-    cuda = (device == 'GPU' and torch.cuda_is_available)
+    cuda = (device == 'GPU' and torch.cuda.is_available)
 
     return torch.device('cuda:0' if cuda else 'cpu')
